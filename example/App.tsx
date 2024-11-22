28e134ec3496e3e3fb1ba4761719bab996a60c3a
Abhishek Singh <abhiwebdev2718@gmail.com>
2024-11-22 16:59:48 +0530

    setup native module

--- example/App.tsx
+++ example/App.tsx
@@ -1,49 +1,15 @@
-import { useEvent } from 'expo';
-import ExpoSettings, { ExpoSettingsView } from 'expo-settings';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { ExpoSettingsView } from "expo-settings";
+import { SafeAreaView } from "react-native";
 
 export default function App() {
-  const onChangePayload = useEvent(ExpoSettings, 'onChange');
-
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{ExpoSettings.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{ExpoSettings.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await ExpoSettings.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <ExpoSettingsView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
-      </ScrollView>
+      <ExpoSettingsView
+        url="https://www.example.com"
+        onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
+        style={styles.view}
+      />
     </SafeAreaView>
-  );
-}
-
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
-    </View>
   );
 }
 
@@ -58,13 +24,13 @@ const styles = {
   },
   group: {
     margin: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
   },
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
   },
   view: {
     flex: 1,
---
+++ ios/SwiftUIButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIButtonView.swift
+//  ExpoSettings
+//
+//  Created by Abhishek Singh on 21/11/24.
+//
+
+import SwiftUI
+
+struct SwiftUIButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIButtonView()
+}
